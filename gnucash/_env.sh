a3950da254cc91f8b8e6e931ec2dcdbfa26b3c71
Boris Frenkel <fobos2k@gmail.com>
2020-07-18 18:29:45 +0300

    - GnuCash: Ubunti bionic. Fix entrypoint     - GnuCash: First release

---
+++ gnucash/_env.sh
@@ -0,0 +1,20 @@
+#!/bin/env bash
+
+# Image parameters
+export IMAGE_NAME=gnucash
+export IMAGE_VERSION=0.0.1
+
+# Container parameters
+export CONTAINER_SUFFIX=container
+
+# User
+export DOCKER_USER=user
+export USER_WORKSPACE=
+
+# External software
+export CMAKE_URL=https://github.com/Kitware/CMake/releases/download/v3.18.0/cmake-3.18.0-Linux-x86_64.sh
+
+# Misc
+export VERBOSE=
+export TEMP=./temp
+export LOGS=./logs
